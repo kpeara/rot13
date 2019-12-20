11d1619232549406c777f7e1a24e26b7f5b4d153
Kunju Perath <kperath@gmail.com>
2019-12-20 15:32:22 -0500

    works! adding in arguments, changing to function

--- rot13.py
+++ rot13.py
@@ -1,19 +1,28 @@
+import sys
+
 file = open("input.txt", "r")
 # take param as any file and generate filename_rot13
+rot13_lines = list()
+lines = file.readlines()
+file.close()
 
-text = file.readlines()
-text_list = list(text) # lists in python are immutable
-sr = ""
+for line in lines:
+    sr = ""
+    for i in line:
+        if i != ' ':
+            if i.islower():
+                asc = (ord(i) + 1) % 123
+                if asc < 97:
+                    asc + 97
+                sr += chr(asc)
+        else:
+            sr += i
+    sr += '\n'
+    rot13_lines.append(sr)
 
-print(text[0])
-for i in text[0]:
-    if i != ' ':
-        if i.islower():
-            asc = (ord(i) + 1) % 123
-            if asc < 97:
-                asc + 97
-            sr += chr(asc)
-    else:
-        sr += i
+print(lines)
+print(rot13_lines)
 
-print(sr)
+file = open("rot13_input.txt", "w")
+for i in rot13_lines:
+    file.write(i)
