6b075fc11fd9d4f3e1657f17c829e76a79a9e304
Kunju Perath <kperath@gmail.com>
2019-12-20 16:24:25 -0500

    can add in any input file

--- rot13.py
+++ rot13.py
@@ -1,28 +1,30 @@
 import sys
+if len(sys.argv) == 2:
+    file = open(sys.argv[1], "r")
+    # take param as any file and generate filename_rot13
+    rot13_lines = list()
+    lines = file.readlines()
+    file.close()
 
-file = open("input.txt", "r")
-# take param as any file and generate filename_rot13
-rot13_lines = list()
-lines = file.readlines()
-file.close()
+    for line in lines:
+        sr = ""
+        for i in line:
+            if i != ' ':
+                if i.islower():
+                    asc = (ord(i) + 1) % 123
+                    if asc < 97:
+                        asc + 97
+                    sr += chr(asc)
+                # is uppper case
+            else:
+                sr += i
+        sr += '\n'
+        rot13_lines.append(sr)
 
-for line in lines:
-    sr = ""
-    for i in line:
-        if i != ' ':
-            if i.islower():
-                asc = (ord(i) + 1) % 123
-                if asc < 97:
-                    asc + 97
-                sr += chr(asc)
-        else:
-            sr += i
-    sr += '\n'
-    rot13_lines.append(sr)
+    print(lines)
+    print(rot13_lines)
 
-print(lines)
-print(rot13_lines)
-
-file = open("rot13_input.txt", "w")
-for i in rot13_lines:
-    file.write(i)
+    file = open("rot13_" + sys.argv[1], "w")
+    for i in rot13_lines:
+        file.write(i)
+    file.close()
